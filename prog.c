97f9bf82a80c9da817852811e4f985f0cb64797d
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-12-07 21:04:52 +0700

    Creating main file
    yeeeeah the main file

---
+++ prog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "charHandler.h"
+
+int main(void) {
+  struct Stack * head = NULL;
+  char chara;
+  while(scanf("%c", &chara)&&chara!='\n'){
+    printf("%clol\n",chara);
+    charHandler(chara, &head);
+  
+  }
+  printf("%d", pop(&head));
+}
